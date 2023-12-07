1cd99541fedab63e42553464cbb2702b1f77b848
Muskan Gautam <152010462+muskan64star@users.noreply.github.com>
2023-12-07 11:57:56 +0530

    Added solution - LeetHub

---
+++ Power Of Numbers - GFG/power-of-numbers.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    #Complete this function
+    def power(self,N,R):
+        #Your code here
+        ans = pow(N,R,int(1e9 + 7))
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        N=input()
+        R=N[::-1]
+        
+        ob=Solution();
+        ans=ob.power(int(N),int(R))
+        print(ans)
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
